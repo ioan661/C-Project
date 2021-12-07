a1699fa975dc8592e3f69b9314fb52b66f17daa5
Lefter Ioan-Alexandru <63010313+ioan661@users.noreply.github.com>
2021-12-07 18:59:35 +0200

    Create header.h

---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H_
+#define HEADER_H_ 
+
+int* makeArray(); 
+void printArray(int* array); 
+void freeArray(int* array); 
+
+
+int** makeMatrix(); 
+void printMatrix(int** matrix); 
+
+#endif 
